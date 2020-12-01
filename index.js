15299102712b35a7ed8a898ccff8ea280b6f2c21
Andrew Chart <andrewc@andertons.co.uk>
2020-12-01 21:51:36 +0000

    Post requests spit out body as json

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.post('/', function (req, res, next) {
+  res.json(req.body)
+})
+
+app.get('*', (req, res) => {
+  res.status(404).send('Not Found')
 })
 
 app.listen(port, () => {
