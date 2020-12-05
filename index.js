dea53a745a567a450594f2b59d89a83f6b734a74
Andrew Chart <andrewc@andertons.co.uk>
2020-12-05 22:57:07 +0000

    Validate the incoming json

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(express.json({ limit: '6mb' }))
-app.use(express.urlencoded({ extended: true, limit: '6mb' }))
+app.use(express.json({ limit: '6mb' }));
+app.use(express.urlencoded({ extended: true, limit: '6mb' }));
 
-app.post('/', function (req, res, next) {
-  if(req.body.apiSecret === process.env.API_SECRET) {
-    res.status(200).json({status: 'SUCCESS', message: 'OK'})
-  } else {
-    res.status(403).json({status: 'ERROR', message: 'Invalid API Key'})
+app.post('/', function (req, res) {
+
+  // Check for API Key Integrity
+  if(req.body.apiSecret !== process.env.API_SECRET) {
+    return res.status(403).json({status: 'ERROR', message: 'Invalid API Key'});
   }
+
+  // Check the payload
+  const validatePayload = require('./modules/validate-payload.js');
+  if(validatePayload(req.body) === false) {
+    return res.status(403).json({status: 'ERROR', message: 'Invalid input data'});
+  }
+
+  // Save the json to a file for asynchronous processing
+  // const saveJson = require('./modules/save-json.js');
+  // saveJson();
+
+  res.status(200).json({status: 'SUCCESS', message: 'OK'});
 })
 
-app.get('*', (req, res) => {
-  res.status(404).send('Not Found')
+app.all('*', (req, res) => {
+  res.status(404).json({status: 'ERROR', message: 'Not Found'})
 })
 
 app.listen(port, () => {
---
+++ modules/validate-payload.js
@@ -0,0 +1,37 @@
+/**
+ * validate-payload.js
+ *
+ * Validates the inbound json data which is POSTed to the API
+ *
+ */
+function validatePayload(data) {
+
+  let expectedKeys = [
+    'activityType',
+    'activityStartTimeIso',
+    'activityNotes',
+    'activityPathLatitude',
+    'activityPathLongitude',
+    'activityPathTimestamp',
+    'activityPathType'
+  ];
+
+  let validPayload = true;
+
+  // Check for presence of all the keys
+  //
+  for(i=0; i<expectedKeys.length; i++) {
+
+    expected = expectedKeys[i];
+
+    if(typeof data[expected] === "undefined") {
+      validPayload = false;
+      break;
+    }
+
+  }
+
+  return validPayload;
+}
+
+module.exports = validatePayload;
